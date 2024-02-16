f45199d9b22160cf0cf4d07277dd16a0dd41d762
maith96 <threedaniel385@gmail.com>
2024-02-16 13:24:49 +0300

    Define table models

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @:click="add">
+      Add
+    </button>
+    <ul>
+      <li v-for="example in examples" :key="example.id">
+        {{ example.id }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const examples = ref<any | null>(null)
+
+async function add () {
+  const example = await $fetch('/api/example/1', {
+    method: 'put',
+    body: { details: 'det', description: 'description' }
+  })
+  console.log('examples: ', example)
+  examples.value = await $fetch('/api/example', {})
+  console.log(examples)
+}
+
+</script>
